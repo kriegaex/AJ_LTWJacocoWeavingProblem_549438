0b465080a83f40afde4d8d26020f6bca1421af6d
Alexander Kriegisch <Alexander@Kriegisch.name>
2021-06-09 08:27:41 +0700

    Initial version, recreating problem

---
+++ src/main/java/de/scrum_master/app/Application.java
@@ -0,0 +1,7 @@
+package de.scrum_master.app;
+
+public class Application {
+  public String greet(String name) {
+    return "Hello " + name;
+  }
+}
---
+++ src/main/java/de/scrum_master/app/MyAspect.java
@@ -0,0 +1,16 @@
+package de.scrum_master.app;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class MyAspect {
+  @Before(
+    "execution(* *(..)) && !within(MyAspect)"
+      //+ " && within(de.scrum_master..*)"
+  )
+  public void myAdvice(JoinPoint joinPoint) {
+    System.out.println(joinPoint);
+  }
+}
